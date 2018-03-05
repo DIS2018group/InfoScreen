c4395a1257d8e92b9e12610fff246e0784955476
Janne Pulkkinen <jannepulk@gmail.com>
2018-03-05 18:59:07 +0200

    Add a call to check user status. Tabs can now have activation/deactivation functions.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 
+import time
+import json
+
 
 app = Flask(__name__)
 
@@ -24,3 +27,24 @@ def main_view():
         "main.html",
         tabs=TABS,
     )
+
+
+@app.route("/heartbeat/")
+def heartbeat():
+    """
+    Probe the environment and figure out which user is logged in at the moment,
+    if any
+    """
+    # TODO: Authentication not implemented yet
+    # For now, fake a login/logout every 5 seconds
+    unix_timestamp = time.time()
+    logged_in = unix_timestamp % 10 >= 5
+
+    data = {
+        "user": {
+            "is_authenticated": True if logged_in else False,
+            "name": "Terry Test" if logged_in else None
+        }
+    }
+
+    return json.dumps(data)
--- static/js/infoscreen.js
+++ static/js/infoscreen.js
@@ -1,29 +1,57 @@
 var self = this;
 
-this.app = null;
-
-this.data = {
+this.infoscreen = {};
+this.infoscreen.data = {
     user: {
         is_authenticated: false,
         name: null
     },
     tabs: [],
-    current_tab: 0
+    current_tab: null
 };
+this.infoscreen.tabs = [];
 
 $(document).ready(function() {
-    self.app = new Vue({
-        el: "#app",
-        data: self.data,
-        methods: {
-            openTab: function(index) {
-                self.data.current_tab = index;
+    var infoscreen = self.infoscreen;
 
-                var tabId = self.data.tabs[index].id;
-                $("#page-area").html($("#" + tabId + "-template").html());
-            }
+    infoscreen.app = new Vue({
+        el: "#app",
+        data: infoscreen.data,
+        methods: {
+            openTab: openTab
         }
     });
 
-    $("#page-area").html($("#unicafe-template").html());
+    // Update user status every 0.5s
+    window.setInterval(function() {
+        updateStatus();
+    }, 500);
+
+    openTab(0);
 });
+
+function openTab(index) {
+    var infoscreen = self.infoscreen;
+
+    // Deactivate current tab (if any)
+    if (infoscreen.data.current_tab !== null) {
+        infoscreen.tabs[infoscreen.data.current_tab].deactivate();
+    }
+
+    infoscreen.data.current_tab = index;
+
+    // Activate the new tab
+    var tabId = infoscreen.data.tabs[index].id;
+    $("#page-area").html($("#" + tabId + "-template").html());
+    self.infoscreen.tabs[index].activate();
+}
+
+function updateStatus() {
+    $.ajax({
+        dataType: "json",
+        url: "/heartbeat/",
+        success: function(data) {
+            infoscreen.data.user = data.user;
+        }
+    });
+}
